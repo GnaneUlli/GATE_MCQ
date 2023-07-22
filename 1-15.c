8e80d4c5ce4d6a7df6897c865b8efa619426247b
Ulli Gnaneswararao <133844134+Gnaneswar484@users.noreply.github.com>
2023-07-22 15:11:34 +0530

    Update 1-15.c

--- 1-15.c
+++ 1-15.c
@@ -7,6 +7,7 @@ int i;
 float j;
 };
 struct node *s[10] ;
+
 (a) An array, each element of which is a pointer to a structure of type
 node
 (b) A structure of 2 fields, each field being a pointer to an array of 10
